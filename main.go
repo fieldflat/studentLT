d4ce16c98934d247bd4e86d47b0ab33cc094555f
Tomonori HIRATA <hirata@sqlab.jp>
2019-10-03 23:18:43 +0900

    データ構造の変更＆Priceで検索可能

--- main.go
+++ main.go
@@ -17,26 +17,31 @@ import (
 // Item は出品アイテム情報
 type Item struct {
 	gorm.Model
-	Title               string `validate:"required"` // イベントタイトル
-	Description         string `validate:"required"` // イベントの説明
-	Price               string `validate:"required"` // イベントの参加価格
-	MaxParticipants     int    `validate:"required"` // イベントの最大参加可能人数
-	NumParticipants     int    // イベントの現在参加予定人数
-	ScheduledDateYear   uint64 `validate:"required"` // イベントの開催予定日時 (年)
-	ScheduledDateMonth  uint64 `validate:"required"` // イベントの開催予定日時 (月)
-	ScheduledDateDay    uint64 `validate:"required"` // イベントの開催予定日時 (日)
-	ScheduledDateHour   uint64 `validate:"required"` // イベントの開催予定日時 (時)
-	ScheduledDateMinute uint64 `validate:"required"` // イベントの開催予定日時 (分)
-	DeadlineDateYear    uint64 `validate:"required"` // 参加申し込みの締切日時 (年)
-	DeadlineDateMonth   uint64 `validate:"required"` // 参加申し込みの締切日時 (月)
-	DeadlineDateDay     uint64 `validate:"required"` // 参加申し込みの締切日時 (日)
-	DeadlineDateHour    uint64 `validate:"required"` // 参加申し込みの締切日時 (時)
-	DeadlineDateMinute  uint64 `validate:"required"` // 参加申し込みの締切日時 (分))
-	Belongings          string `validate:"required"` // 持ち物リスト
-	Target              string `validate:"required"` // 参加対象者
-	Other               string // その他
-	CreatedTime         string `validate:"required"` // 作成日時
-	UpdatedTime         string `validate:"required"` // 更新日時
+	Title                  string `validate:"required"` // イベントタイトル
+	Description            string `validate:"required"` // イベントの説明
+	Price                  int    // イベントの参加価格
+	MaxParticipants        int    // イベントの最大参加可能人数
+	NumParticipants        int    // イベントの現在参加予定人数
+	ScheduledDateYear      int    // イベントの開催予定日時 (年)
+	ScheduledDateMonth     int    // イベントの開催予定日時 (月)
+	ScheduledDateDay       int    // イベントの開催予定日時 (日)
+	ScheduledDateHour      int    // イベントの開催予定日時 (時)
+	ScheduledDateMinute    int    // イベントの開催予定日時 (分)
+	ScheduledDateEndYear   int    // イベントの開催終了日時 (年)
+	ScheduledDateEndMonth  int    // イベントの開催終了日時 (月)
+	ScheduledDateEndDay    int    // イベントの開催終了日時 (日)
+	ScheduledDateEndHour   int    // イベントの開催終了日時 (時)
+	ScheduledDateEndMinute int    // イベントの開催終了日時 (分)
+	DeadlineDateYear       int    // 参加申し込みの締切日時 (年)
+	DeadlineDateMonth      int    // 参加申し込みの締切日時 (月)
+	DeadlineDateDay        int    // 参加申し込みの締切日時 (日)
+	DeadlineDateHour       int    // 参加申し込みの締切日時 (時)
+	DeadlineDateMinute     int    // 参加申し込みの締切日時 (分))
+	Belongings             string // 持ち物リスト
+	Target                 string // 参加対象者
+	Other                  string // その他
+	CreatedTime            string `validate:"required"` // 作成日時
+	UpdatedTime            string `validate:"required"` // 更新日時
 }
 
 // Validate about Item structure.
@@ -57,36 +62,6 @@ func (form *Item) Validate() (ok bool, result map[string]string) {
 					result["Title"] = "タイトルの入力は必須です．"
 				case "Description":
 					result["Description"] = "本文の入力は必須です．"
-				case "Price":
-					result["Price"] = "点数の入力は必須です．"
-				case "MaxParticipants":
-					result["MaxParticipants"] = "最大参加人数の入力は必須です．"
-				case "NumParticipants":
-					result["NumParticipants"] = "現在参加人数の入力は必須です．"
-				case "ScheduledDateYear":
-					result["ScheduledDateYear"] = "開催予定日時(年)の入力は必須です．"
-				case "ScheduledDateMonth":
-					result["ScheduledDateMonth"] = "開催予定日時(月)の入力は必須です．"
-				case "ScheduledDateDay":
-					result["ScheduledDateDay"] = "開催予定日時(日)の入力は必須です．"
-				case "ScheduledDateHour":
-					result["ScheduledDateHour"] = "開催予定日時(時)の入力は必須です．"
-				case "ScheduledDateMinute":
-					result["ScheduledDateMinute"] = "開催予定日時(分)の入力は必須です．"
-				case "DeadlineDateYear":
-					result["DeadlineDateYear"] = "申し込み締切日時(年)の入力は必須です．"
-				case "DeadlineDateMonth":
-					result["DeadlineDateMonth"] = "申し込み締切日時(月)の入力は必須です．"
-				case "DeadlineDateDay":
-					result["DeadlineDateDay"] = "申し込み締切日時(日)の入力は必須です．"
-				case "DeadlineDateHour":
-					result["DeadlineDateHour"] = "申し込み締切日時(時))の入力は必須です．"
-				case "DeadlineDateMinute":
-					result["DeadlineDateMinute"] = "申し込み締切日時(分)の入力は必須です．"
-				case "Belongings":
-					result["Belongings"] = "持ち物の入力は必須です．"
-				case "Target":
-					result["Target"] = "参加対象の入力は必須です．"
 				case "CreatedTime":
 					result["CreatedTime"] = "CreatedTimeの入力は必須です．"
 				case "UpdatedTime":
@@ -113,19 +88,24 @@ func dbInit() {
 func create(
 	title string,
 	description string,
-	price string,
+	price int,
 	maxParticipants int,
 	numParticipants int,
-	scheduledDateYear uint64,
-	scheduledDateMonth uint64,
-	scheduledDateDay uint64,
-	scheduledDateHour uint64,
-	scheduledDateMinute uint64,
-	deadlineDateYear uint64,
-	deadlineDateMonth uint64,
-	deadlineDateDay uint64,
-	deadlineDateHour uint64,
-	deadlineDateMinute uint64,
+	scheduledDateYear int,
+	scheduledDateMonth int,
+	scheduledDateDay int,
+	scheduledDateHour int,
+	scheduledDateMinute int,
+	scheduledDateEndYear int,
+	scheduledDateEndMonth int,
+	scheduledDateEndDay int,
+	scheduledDateEndHour int,
+	scheduledDateEndMinute int,
+	deadlineDateYear int,
+	deadlineDateMonth int,
+	deadlineDateDay int,
+	deadlineDateHour int,
+	deadlineDateMinute int,
 	belongings string,
 	target string,
 	other string,
@@ -139,30 +119,34 @@ func create(
 
 	// 処理を追加
 	form := Item{
-		Title:               title,
-		Description:         description,
-		Price:               price,
-		MaxParticipants:     maxParticipants,
-		NumParticipants:     numParticipants,
-		ScheduledDateYear:   scheduledDateYear,
-		ScheduledDateMonth:  scheduledDateMonth,
-		ScheduledDateDay:    scheduledDateDay,
-		ScheduledDateHour:   scheduledDateHour,
-		ScheduledDateMinute: scheduledDateMinute,
-		DeadlineDateYear:    deadlineDateYear,
-		DeadlineDateMonth:   deadlineDateMonth,
-		DeadlineDateDay:     deadlineDateDay,
-		DeadlineDateHour:    deadlineDateHour,
-		DeadlineDateMinute:  deadlineDateMinute,
-		Belongings:          belongings,
-		Target:              target,
-		Other:               other,
-		CreatedTime:         createdTime,
-		UpdatedTime:         updatedTime,
+		Title:                  title,
+		Description:            description,
+		Price:                  price,
+		MaxParticipants:        maxParticipants,
+		NumParticipants:        numParticipants,
+		ScheduledDateYear:      scheduledDateYear,
+		ScheduledDateMonth:     scheduledDateMonth,
+		ScheduledDateDay:       scheduledDateDay,
+		ScheduledDateHour:      scheduledDateHour,
+		ScheduledDateMinute:    scheduledDateMinute,
+		ScheduledDateEndYear:   scheduledDateEndYear,
+		ScheduledDateEndMonth:  scheduledDateEndMonth,
+		ScheduledDateEndDay:    scheduledDateEndDay,
+		ScheduledDateEndHour:   scheduledDateEndHour,
+		ScheduledDateEndMinute: scheduledDateEndMinute,
+		DeadlineDateYear:       deadlineDateYear,
+		DeadlineDateMonth:      deadlineDateMonth,
+		DeadlineDateDay:        deadlineDateDay,
+		DeadlineDateHour:       deadlineDateHour,
+		DeadlineDateMinute:     deadlineDateMinute,
+		Belongings:             belongings,
+		Target:                 target,
+		Other:                  other,
+		CreatedTime:            createdTime,
+		UpdatedTime:            updatedTime,
 	}
 
-	log.Print("MaxParticipants: ", maxParticipants)
-	log.Print("NumParticipants: ", numParticipants)
+	log.Print(form)
 
 	// バリデーションの検証を行う
 	ok, errorMessages := form.Validate()
@@ -174,26 +158,31 @@ func create(
 
 	log.Print("入力エラーなし！！")
 	db.Create(&Item{
-		Title:               title,
-		Description:         description,
-		Price:               price,
-		MaxParticipants:     maxParticipants,
-		NumParticipants:     numParticipants,
-		ScheduledDateYear:   scheduledDateYear,
-		ScheduledDateMonth:  scheduledDateMonth,
-		ScheduledDateDay:    scheduledDateDay,
-		ScheduledDateHour:   scheduledDateHour,
-		ScheduledDateMinute: scheduledDateMinute,
-		DeadlineDateYear:    deadlineDateYear,
-		DeadlineDateMonth:   deadlineDateMonth,
-		DeadlineDateDay:     deadlineDateDay,
-		DeadlineDateHour:    deadlineDateHour,
-		DeadlineDateMinute:  deadlineDateMinute,
-		Belongings:          belongings,
-		Target:              target,
-		Other:               other,
-		CreatedTime:         createdTime,
-		UpdatedTime:         updatedTime,
+		Title:                  title,
+		Description:            description,
+		Price:                  price,
+		MaxParticipants:        maxParticipants,
+		NumParticipants:        numParticipants,
+		ScheduledDateYear:      scheduledDateYear,
+		ScheduledDateMonth:     scheduledDateMonth,
+		ScheduledDateDay:       scheduledDateDay,
+		ScheduledDateHour:      scheduledDateHour,
+		ScheduledDateMinute:    scheduledDateMinute,
+		ScheduledDateEndYear:   scheduledDateEndYear,
+		ScheduledDateEndMonth:  scheduledDateEndMonth,
+		ScheduledDateEndDay:    scheduledDateEndDay,
+		ScheduledDateEndHour:   scheduledDateEndHour,
+		ScheduledDateEndMinute: scheduledDateEndMinute,
+		DeadlineDateYear:       deadlineDateYear,
+		DeadlineDateMonth:      deadlineDateMonth,
+		DeadlineDateDay:        deadlineDateDay,
+		DeadlineDateHour:       deadlineDateHour,
+		DeadlineDateMinute:     deadlineDateMinute,
+		Belongings:             belongings,
+		Target:                 target,
+		Other:                  other,
+		CreatedTime:            createdTime,
+		UpdatedTime:            updatedTime,
 	})
 	return errorMessages
 }
@@ -202,19 +191,24 @@ func update(
 	id int,
 	title string,
 	description string,
-	price string,
+	price int,
 	maxParticipants int,
 	numParticipants int,
-	scheduledDateYear uint64,
-	scheduledDateMonth uint64,
-	scheduledDateDay uint64,
-	scheduledDateHour uint64,
-	scheduledDateMinute uint64,
-	deadlineDateYear uint64,
-	deadlineDateMonth uint64,
-	deadlineDateDay uint64,
-	deadlineDateHour uint64,
-	deadlineDateMinute uint64,
+	scheduledDateYear int,
+	scheduledDateMonth int,
+	scheduledDateDay int,
+	scheduledDateHour int,
+	scheduledDateMinute int,
+	scheduledDateEndYear int,
+	scheduledDateEndMonth int,
+	scheduledDateEndDay int,
+	scheduledDateEndHour int,
+	scheduledDateEndMinute int,
+	deadlineDateYear int,
+	deadlineDateMonth int,
+	deadlineDateDay int,
+	deadlineDateHour int,
+	deadlineDateMinute int,
 	belongings string,
 	target string,
 	other string,
@@ -237,6 +231,11 @@ func update(
 	item.ScheduledDateDay = scheduledDateDay
 	item.ScheduledDateHour = scheduledDateHour
 	item.ScheduledDateMinute = scheduledDateMinute
+	item.ScheduledDateEndYear = scheduledDateEndYear
+	item.ScheduledDateEndMonth = scheduledDateEndMonth
+	item.ScheduledDateEndDay = scheduledDateEndDay
+	item.ScheduledDateEndHour = scheduledDateEndHour
+	item.ScheduledDateEndMinute = scheduledDateEndMinute
 	item.DeadlineDateYear = deadlineDateYear
 	item.DeadlineDateMonth = deadlineDateMonth
 	item.DeadlineDateDay = deadlineDateDay
@@ -270,30 +269,46 @@ func search(searchWords map[string]string) []Item {
 	}
 	var items []Item
 	query := db.Order("created_at desc")
+	log.Print(searchWords)
 
 	if len(searchWords["words"]) != 0 {
 		query = query.Where("title LIKE (?) OR description LIKE (?)", "%"+searchWords["words"]+"%", "%"+searchWords["words"]+"%")
 	}
 
-	if searchWords["scheduledDateFrom"] != "" || searchWords["scheduledDateTo"] != "" {
+	if searchWords["scheduledDateFrom"] != "" {
 		scheduledDateFrom := searchWords["scheduledDateFrom"]
 		scheduledDateFromSlice := strings.Split(scheduledDateFrom, "-")
 		scheduledDateFromYear, _ := strconv.ParseUint(scheduledDateFromSlice[0], 10, 32)
 		scheduledDateFromMonth, _ := strconv.ParseUint(scheduledDateFromSlice[1], 10, 32)
 		scheduledDateFromDay, _ := strconv.ParseUint(scheduledDateFromSlice[2], 10, 32)
 
+		query = query.Where("scheduled_date_end_year >= (?) ", scheduledDateFromYear)
+		query = query.Where("scheduled_date_end_month >= (?) ", scheduledDateFromMonth)
+		query = query.Where("scheduled_date_end_day >= (?) ", scheduledDateFromDay)
+	}
+
+	if searchWords["scheduledDateTo"] != "" {
 		scheduledDateTo := searchWords["scheduledDateTo"]
 		scheduledDateToSlice := strings.Split(scheduledDateTo, "-")
 		scheduledDateToYear, _ := strconv.ParseUint(scheduledDateToSlice[0], 10, 32)
 		scheduledDateToMonth, _ := strconv.ParseUint(scheduledDateToSlice[1], 10, 32)
 		scheduledDateToDay, _ := strconv.ParseUint(scheduledDateToSlice[2], 10, 32)
 
-		query = query.Where("scheduled_date_year >= (?) ", scheduledDateFromYear)
 		query = query.Where("scheduled_date_year <= (?) ", scheduledDateToYear)
-		query = query.Where("scheduled_date_month >= (?) ", scheduledDateFromMonth)
 		query = query.Where("scheduled_date_month <= (?) ", scheduledDateToMonth)
-		query = query.Where("scheduled_date_day >= (?) ", scheduledDateFromDay)
 		query = query.Where("scheduled_date_day <= (?) ", scheduledDateToDay)
+	}
+
+	if searchWords["priceFrom"] != "" {
+		priceFrom, _ := strconv.Atoi(searchWords["priceFrom"])
+		log.Print("priceFrom = ", priceFrom)
+		query = query.Where("price >= (?) ", priceFrom)
+	}
+
+	if searchWords["priceTo"] != "" {
+		priceTo, _ := strconv.Atoi(searchWords["priceTo"])
+		log.Print("priceTo = ", priceTo)
+		query = query.Where("price <= (?) ", priceTo)
 	}
 
 	query.Find(&items)
@@ -325,10 +340,8 @@ func main() {
 		searchWords["words"] = c.Query("words")
 		searchWords["scheduledDateFrom"] = c.Query("scheduledDateFrom")
 		searchWords["scheduledDateTo"] = c.Query("scheduledDateTo")
-
-		log.Print("searchWords['words']: ", searchWords["words"])
-		log.Print("searchWords['scheduledDateFrom']: ", searchWords["words"])
-		log.Print("searchWords['scheduledDateTo']: ", searchWords["words"])
+		searchWords["priceFrom"] = c.Query("priceFrom")
+		searchWords["priceTo"] = c.Query("priceTo")
 
 		items := search(searchWords)
 		c.HTML(200, "index.tmpl", gin.H{
@@ -347,31 +360,42 @@ func main() {
 	r.POST("/ughfkhszdlvjkdjsbfkjsdabfl/sadmin/create", func(c *gin.Context) {
 		title := c.PostForm("title")
 		description := c.PostForm("description")
-		price := c.PostForm("price")
+		price, _ := strconv.Atoi(c.PostForm("price"))
 		maxParticipants, _ := strconv.Atoi(c.PostForm("maxParticipants"))
 		numParticipants := 0
 
 		scheduledDate := c.PostForm("scheduledDate")
 		scheduledDateSlice := strings.Split(scheduledDate, "-")
-		scheduledDateYear, _ := strconv.ParseUint(scheduledDateSlice[0], 10, 32)
-		scheduledDateMonth, _ := strconv.ParseUint(scheduledDateSlice[1], 10, 32)
-		scheduledDateDay, _ := strconv.ParseUint(scheduledDateSlice[2], 10, 32)
+		scheduledDateYear, _ := strconv.Atoi(scheduledDateSlice[0])
+		scheduledDateMonth, _ := strconv.Atoi(scheduledDateSlice[1])
+		scheduledDateDay, _ := strconv.Atoi(scheduledDateSlice[2])
 
 		scheduledTime := c.PostForm("scheduledTime")
 		scheduledTimeSlice := strings.Split(scheduledTime, ":")
-		scheduledDateHour, _ := strconv.ParseUint(scheduledTimeSlice[0], 10, 32)
-		scheduledDateMinute, _ := strconv.ParseUint(scheduledTimeSlice[1], 10, 32)
+		scheduledDateHour, _ := strconv.Atoi(scheduledTimeSlice[0])
+		scheduledDateMinute, _ := strconv.Atoi(scheduledTimeSlice[1])
+
+		scheduledDateEnd := c.PostForm("scheduledDateEnd")
+		scheduledDateEndSlice := strings.Split(scheduledDateEnd, "-")
+		scheduledDateEndYear, _ := strconv.Atoi(scheduledDateEndSlice[0])
+		scheduledDateEndMonth, _ := strconv.Atoi(scheduledDateEndSlice[1])
+		scheduledDateEndDay, _ := strconv.Atoi(scheduledDateEndSlice[2])
+
+		scheduledEndTime := c.PostForm("scheduledEndTime")
+		scheduledEndTimeSlice := strings.Split(scheduledEndTime, ":")
+		scheduledDateEndHour, _ := strconv.Atoi(scheduledEndTimeSlice[0])
+		scheduledDateEndMinute, _ := strconv.Atoi(scheduledEndTimeSlice[1])
 
 		deadlineDate := c.PostForm("deadlineDate")
 		deadlineDateSlice := strings.Split(deadlineDate, "-")
-		deadlineDateYear, _ := strconv.ParseUint(deadlineDateSlice[0], 10, 32)
-		deadlineDateMonth, _ := strconv.ParseUint(deadlineDateSlice[1], 10, 32)
-		deadlineDateDay, _ := strconv.ParseUint(deadlineDateSlice[2], 10, 32)
+		deadlineDateYear, _ := strconv.Atoi(deadlineDateSlice[0])
+		deadlineDateMonth, _ := strconv.Atoi(deadlineDateSlice[1])
+		deadlineDateDay, _ := strconv.Atoi(deadlineDateSlice[2])
 
 		deadlineTime := c.PostForm("deadlineTime")
 		deadlineTimeSlice := strings.Split(deadlineTime, ":")
-		deadlineDateHour, _ := strconv.ParseUint(deadlineTimeSlice[0], 10, 32)
-		deadlineDateMinute, _ := strconv.ParseUint(deadlineTimeSlice[1], 10, 32)
+		deadlineDateHour, _ := strconv.Atoi(deadlineTimeSlice[0])
+		deadlineDateMinute, _ := strconv.Atoi(deadlineTimeSlice[1])
 
 		belongings := c.PostForm("belongings")
 		target := c.PostForm("target")
@@ -390,6 +414,11 @@ func main() {
 			scheduledDateDay,
 			scheduledDateHour,
 			scheduledDateMinute,
+			scheduledDateEndYear,
+			scheduledDateEndMonth,
+			scheduledDateEndDay,
+			scheduledDateEndHour,
+			scheduledDateEndMinute,
 			deadlineDateYear,
 			deadlineDateMonth,
 			deadlineDateDay,
@@ -401,7 +430,44 @@ func main() {
 			createdTime,
 			updatedTime,
 		)
-		c.Redirect(302, "/")
+		c.Redirect(302, "/ughfkhszdlvjkdjsbfkjsdabfl/sadmin")
+	})
+
+	// sadmin Editページ
+	r.GET("/ughfkhszdlvjkdjsbfkjsdabfl/edit", func(c *gin.Context) {
+		r.LoadHTMLGlob("templates/sadmin/*")
+		db, err := gorm.Open("sqlite3", "test.sqlite3")
+		if err != nil {
+			panic("failed to connect database\n")
+		}
+
+		var item Item
+		id := c.Query("id")
+		db.First(&item, id)
+
+		if item.ID == 0 {
+			log.Print("item.ID = ", item.ID)
+			c.Redirect(302, "/ughfkhszdlvjkdjsbfkjsdabfl/sadmin")
+		} else {
+
+			scheduledDate := strconv.Itoa(item.ScheduledDateYear) + "-" + strconv.Itoa(item.ScheduledDateMonth) + "-" + strconv.Itoa(item.ScheduledDateDay)
+			scheduledTime := strconv.Itoa(item.ScheduledDateHour) + ":" + strconv.Itoa(item.ScheduledDateMinute)
+			scheduledDateEnd := strconv.Itoa(item.ScheduledDateEndYear) + "-" + strconv.Itoa(item.ScheduledDateEndMonth) + "-" + strconv.Itoa(item.ScheduledDateEndDay)
+			scheduledEndTime := strconv.Itoa(item.ScheduledDateEndHour) + ":" + strconv.Itoa(item.ScheduledDateEndMinute)
+			deadlineDate := strconv.Itoa(item.DeadlineDateYear) + "-" + strconv.Itoa(item.DeadlineDateMonth) + "-" + strconv.Itoa(item.DeadlineDateDay)
+			deadlineTime := strconv.Itoa(item.DeadlineDateHour) + ":" + strconv.Itoa(item.DeadlineDateMinute)
+			log.Print("scheduledDate = ", scheduledDate)
+
+			c.HTML(200, "sadmin_index.tmpl", gin.H{
+				"item":             item,
+				"scheduledDate":    scheduledDate,
+				"scheduledTime":    scheduledTime,
+				"scheduledDateEnd": scheduledDateEnd,
+				"scheduledEndTime": scheduledEndTime,
+				"deadlineDate":     deadlineDate,
+				"deadlineTime":     deadlineTime,
+			})
+		}
 	})
 
 	// assets フォルダの読み取り
