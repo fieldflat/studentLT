ffd3cfb9f961c566bc75f2dc448e39ffd11e1099
Tomonori HIRATA <hirata@sqlab.jp>
2019-10-12 21:15:15 +0900

    create & update のリファクタリング

--- main.go
+++ main.go
@@ -44,7 +44,18 @@ type Item struct {
 	UpdatedTime            string `validate:"required"` // 更新日時
 }
 
+// User はユーザ情報
+type User struct {
+	// 大文字だと Public 扱い
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 // Validate about Item structure.
+// ==============================
+// 構造体Itemのバリデーション
+// ==============================
 func (form *Item) Validate() (ok bool, result map[string]string) {
 	result = make(map[string]string)
 	// 構造体のデータをタグで定義した検証方法でチェック
@@ -74,7 +85,10 @@ func (form *Item) Validate() (ok bool, result map[string]string) {
 	return true, result
 }
 
+// ======================
 // DBの初期化処理
+// Initialization of DB
+// ======================
 func dbInit() {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
@@ -84,72 +98,17 @@ func dbInit() {
 	db.AutoMigrate(&Item{})
 }
 
+// ===================
 // create関数
-func create(
-	title string,
-	description string,
-	price int,
-	maxParticipants int,
-	numParticipants int,
-	scheduledDateYear int,
-	scheduledDateMonth int,
-	scheduledDateDay int,
-	scheduledDateHour int,
-	scheduledDateMinute int,
-	scheduledDateEndYear int,
-	scheduledDateEndMonth int,
-	scheduledDateEndDay int,
-	scheduledDateEndHour int,
-	scheduledDateEndMinute int,
-	deadlineDateYear int,
-	deadlineDateMonth int,
-	deadlineDateDay int,
-	deadlineDateHour int,
-	deadlineDateMinute int,
-	belongings string,
-	target string,
-	other string,
-	createdTime string,
-	updatedTime string,
-) map[string]string {
+// ===================
+func create(item Item) map[string]string {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
 		panic("failed to connect database\n")
 	}
 
-	// 処理を追加
-	form := Item{
-		Title:                  title,
-		Description:            description,
-		Price:                  price,
-		MaxParticipants:        maxParticipants,
-		NumParticipants:        numParticipants,
-		ScheduledDateYear:      scheduledDateYear,
-		ScheduledDateMonth:     scheduledDateMonth,
-		ScheduledDateDay:       scheduledDateDay,
-		ScheduledDateHour:      scheduledDateHour,
-		ScheduledDateMinute:    scheduledDateMinute,
-		ScheduledDateEndYear:   scheduledDateEndYear,
-		ScheduledDateEndMonth:  scheduledDateEndMonth,
-		ScheduledDateEndDay:    scheduledDateEndDay,
-		ScheduledDateEndHour:   scheduledDateEndHour,
-		ScheduledDateEndMinute: scheduledDateEndMinute,
-		DeadlineDateYear:       deadlineDateYear,
-		DeadlineDateMonth:      deadlineDateMonth,
-		DeadlineDateDay:        deadlineDateDay,
-		DeadlineDateHour:       deadlineDateHour,
-		DeadlineDateMinute:     deadlineDateMinute,
-		Belongings:             belongings,
-		Target:                 target,
-		Other:                  other,
-		CreatedTime:            createdTime,
-		UpdatedTime:            updatedTime,
-	}
-
-	log.Print(form)
-
 	// バリデーションの検証を行う
-	ok, errorMessages := form.Validate()
+	ok, errorMessages := item.Validate()
 	if !ok {
 		log.Print("入力エラーあり")
 		log.Print(errorMessages)
@@ -157,100 +116,26 @@ func create(
 	}
 
 	log.Print("入力エラーなし！！")
-	db.Create(&Item{
-		Title:                  title,
-		Description:            description,
-		Price:                  price,
-		MaxParticipants:        maxParticipants,
-		NumParticipants:        numParticipants,
-		ScheduledDateYear:      scheduledDateYear,
-		ScheduledDateMonth:     scheduledDateMonth,
-		ScheduledDateDay:       scheduledDateDay,
-		ScheduledDateHour:      scheduledDateHour,
-		ScheduledDateMinute:    scheduledDateMinute,
-		ScheduledDateEndYear:   scheduledDateEndYear,
-		ScheduledDateEndMonth:  scheduledDateEndMonth,
-		ScheduledDateEndDay:    scheduledDateEndDay,
-		ScheduledDateEndHour:   scheduledDateEndHour,
-		ScheduledDateEndMinute: scheduledDateEndMinute,
-		DeadlineDateYear:       deadlineDateYear,
-		DeadlineDateMonth:      deadlineDateMonth,
-		DeadlineDateDay:        deadlineDateDay,
-		DeadlineDateHour:       deadlineDateHour,
-		DeadlineDateMinute:     deadlineDateMinute,
-		Belongings:             belongings,
-		Target:                 target,
-		Other:                  other,
-		CreatedTime:            createdTime,
-		UpdatedTime:            updatedTime,
-	})
+	db.Create(&item)
 	return errorMessages
 }
 
-func update(
-	id int,
-	title string,
-	description string,
-	price int,
-	maxParticipants int,
-	numParticipants int,
-	scheduledDateYear int,
-	scheduledDateMonth int,
-	scheduledDateDay int,
-	scheduledDateHour int,
-	scheduledDateMinute int,
-	scheduledDateEndYear int,
-	scheduledDateEndMonth int,
-	scheduledDateEndDay int,
-	scheduledDateEndHour int,
-	scheduledDateEndMinute int,
-	deadlineDateYear int,
-	deadlineDateMonth int,
-	deadlineDateDay int,
-	deadlineDateHour int,
-	deadlineDateMinute int,
-	belongings string,
-	target string,
-	other string,
-	createdTime string,
-	updatedTime string,
-) {
+// ====================
+// update関数
+// ====================
+func update(item Item) {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
 		panic("failed to connect database\n")
 	}
-	var item Item
-	db.First(&item, id)
-	item.Title = title
-	item.Description = description
-	item.Price = price
-	item.MaxParticipants = maxParticipants
-	item.NumParticipants = numParticipants
-	item.ScheduledDateYear = scheduledDateYear
-	item.ScheduledDateMonth = scheduledDateMonth
-	item.ScheduledDateDay = scheduledDateDay
-	item.ScheduledDateHour = scheduledDateHour
-	item.ScheduledDateMinute = scheduledDateMinute
-	item.ScheduledDateEndYear = scheduledDateEndYear
-	item.ScheduledDateEndMonth = scheduledDateEndMonth
-	item.ScheduledDateEndDay = scheduledDateEndDay
-	item.ScheduledDateEndHour = scheduledDateEndHour
-	item.ScheduledDateEndMinute = scheduledDateEndMinute
-	item.DeadlineDateYear = deadlineDateYear
-	item.DeadlineDateMonth = deadlineDateMonth
-	item.DeadlineDateDay = deadlineDateDay
-	item.DeadlineDateHour = deadlineDateHour
-	item.DeadlineDateMinute = deadlineDateMinute
-	item.Belongings = belongings
-	item.Target = target
-	item.Other = other
-	item.CreatedTime = createdTime
-	item.UpdatedTime = updatedTime
 	db.Save(&item)
 	db.Close()
 }
 
+// ====================
+// getAll関数
 // 全てのItemを取得する
+// ====================
 func getAll() []Item {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
@@ -261,7 +146,10 @@ func getAll() []Item {
 	return items
 }
 
+// =============================
+// search関数
 // 検索条件を満たすItemを取得する
+// =============================
 func search(searchWords map[string]string) []Item {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
@@ -315,15 +203,18 @@ func search(searchWords map[string]string) []Item {
 	return items
 }
 
-//
+// ================
 // main関数
-//
+// ================
 func main() {
 	r := gin.Default()
 	r.Static("/assets", "./assets")
 	dbInit()
 
+	// *********************
+	// url: "/"
 	// 一覧取得
+	// *********************
 	r.GET("/", func(c *gin.Context) {
 		r.LoadHTMLGlob("templates/main/*")
 		items := getAll()
@@ -332,7 +223,10 @@ func main() {
 		})
 	})
 
+	// *********************
+	// url: "/search"
 	// 検索結果取得
+	// *********************
 	r.GET("/search", func(c *gin.Context) {
 		r.LoadHTMLGlob("templates/main/*")
 		// var searchWords map[string]string
@@ -350,90 +244,101 @@ func main() {
 		})
 	})
 
+	// *********************
+	// url: "/detail"
+	// 詳細ページ
+	// *********************
+	r.GET("/detail", func(c *gin.Context) {
+		r.LoadHTMLGlob("templates/main/*")
+		db, err := gorm.Open("sqlite3", "test.sqlite3")
+		if err != nil {
+			panic("failed to connect database\n")
+		}
+
+		var item Item
+		id := c.Query("id")
+		db.First(&item, id)
+
+		c.HTML(200, "detail.tmpl", gin.H{
+			"item": item,
+		})
+	})
+
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//
+	// 以下，sadminの処理
+	//
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	// *******************************************
+	// url: "/ughfkhszdlvjkdjsbfkjsdabfl/sadmin"
 	// sadmin TOPページ
+	// *******************************************
 	r.GET("/ughfkhszdlvjkdjsbfkjsdabfl/sadmin", func(c *gin.Context) {
 		r.LoadHTMLGlob("templates/sadmin/*")
 		c.HTML(200, "sadmin_index.tmpl", gin.H{})
 	})
 
+	// *************************************************
+	// url: "/ughfkhszdlvjkdjsbfkjsdabfl/sadmin/create"
 	// sadmin イベント情報作成ページ
+	// *************************************************
 	r.POST("/ughfkhszdlvjkdjsbfkjsdabfl/sadmin/create", func(c *gin.Context) {
-		title := c.PostForm("title")
-		description := c.PostForm("description")
-		price, _ := strconv.Atoi(c.PostForm("price"))
-		maxParticipants, _ := strconv.Atoi(c.PostForm("maxParticipants"))
-		numParticipants := 0
+		item := Item{}
+
+		item.Title = c.PostForm("title")
+		item.Description = c.PostForm("description")
+		item.Price, _ = strconv.Atoi(c.PostForm("price"))
+		item.MaxParticipants, _ = strconv.Atoi(c.PostForm("maxParticipants"))
+		item.NumParticipants = 0
 
 		scheduledDate := c.PostForm("scheduledDate")
 		scheduledDateSlice := strings.Split(scheduledDate, "-")
-		scheduledDateYear, _ := strconv.Atoi(scheduledDateSlice[0])
-		scheduledDateMonth, _ := strconv.Atoi(scheduledDateSlice[1])
-		scheduledDateDay, _ := strconv.Atoi(scheduledDateSlice[2])
+		item.ScheduledDateYear, _ = strconv.Atoi(scheduledDateSlice[0])
+		item.ScheduledDateMonth, _ = strconv.Atoi(scheduledDateSlice[1])
+		item.ScheduledDateDay, _ = strconv.Atoi(scheduledDateSlice[2])
 
 		scheduledTime := c.PostForm("scheduledTime")
 		scheduledTimeSlice := strings.Split(scheduledTime, ":")
-		scheduledDateHour, _ := strconv.Atoi(scheduledTimeSlice[0])
-		scheduledDateMinute, _ := strconv.Atoi(scheduledTimeSlice[1])
+		item.ScheduledDateHour, _ = strconv.Atoi(scheduledTimeSlice[0])
+		item.ScheduledDateMinute, _ = strconv.Atoi(scheduledTimeSlice[1])
 
 		scheduledDateEnd := c.PostForm("scheduledDateEnd")
 		scheduledDateEndSlice := strings.Split(scheduledDateEnd, "-")
-		scheduledDateEndYear, _ := strconv.Atoi(scheduledDateEndSlice[0])
-		scheduledDateEndMonth, _ := strconv.Atoi(scheduledDateEndSlice[1])
-		scheduledDateEndDay, _ := strconv.Atoi(scheduledDateEndSlice[2])
+		item.ScheduledDateEndYear, _ = strconv.Atoi(scheduledDateEndSlice[0])
+		item.ScheduledDateEndMonth, _ = strconv.Atoi(scheduledDateEndSlice[1])
+		item.ScheduledDateEndDay, _ = strconv.Atoi(scheduledDateEndSlice[2])
 
 		scheduledEndTime := c.PostForm("scheduledEndTime")
 		scheduledEndTimeSlice := strings.Split(scheduledEndTime, ":")
-		scheduledDateEndHour, _ := strconv.Atoi(scheduledEndTimeSlice[0])
-		scheduledDateEndMinute, _ := strconv.Atoi(scheduledEndTimeSlice[1])
+		item.ScheduledDateEndHour, _ = strconv.Atoi(scheduledEndTimeSlice[0])
+		item.ScheduledDateEndMinute, _ = strconv.Atoi(scheduledEndTimeSlice[1])
 
 		deadlineDate := c.PostForm("deadlineDate")
 		deadlineDateSlice := strings.Split(deadlineDate, "-")
-		deadlineDateYear, _ := strconv.Atoi(deadlineDateSlice[0])
-		deadlineDateMonth, _ := strconv.Atoi(deadlineDateSlice[1])
-		deadlineDateDay, _ := strconv.Atoi(deadlineDateSlice[2])
+		item.DeadlineDateYear, _ = strconv.Atoi(deadlineDateSlice[0])
+		item.DeadlineDateMonth, _ = strconv.Atoi(deadlineDateSlice[1])
+		item.DeadlineDateDay, _ = strconv.Atoi(deadlineDateSlice[2])
 
 		deadlineTime := c.PostForm("deadlineTime")
 		deadlineTimeSlice := strings.Split(deadlineTime, ":")
-		deadlineDateHour, _ := strconv.Atoi(deadlineTimeSlice[0])
-		deadlineDateMinute, _ := strconv.Atoi(deadlineTimeSlice[1])
+		item.DeadlineDateHour, _ = strconv.Atoi(deadlineTimeSlice[0])
+		item.DeadlineDateMinute, _ = strconv.Atoi(deadlineTimeSlice[1])
 
-		belongings := c.PostForm("belongings")
-		target := c.PostForm("target")
-		other := c.PostForm("other")
-		createdTime := time.Now().Format("2006/1/2 15:04:05")
-		updatedTime := time.Now().Format("2006/1/2 15:04:05")
+		item.Belongings = c.PostForm("belongings")
+		item.Target = c.PostForm("target")
+		item.Other = c.PostForm("other")
+		item.CreatedTime = time.Now().Format("2006/1/2 15:04:05")
+		item.UpdatedTime = time.Now().Format("2006/1/2 15:04:05")
 
-		create(
-			title,
-			description,
-			price,
-			maxParticipants,
-			numParticipants,
-			scheduledDateYear,
-			scheduledDateMonth,
-			scheduledDateDay,
-			scheduledDateHour,
-			scheduledDateMinute,
-			scheduledDateEndYear,
-			scheduledDateEndMonth,
-			scheduledDateEndDay,
-			scheduledDateEndHour,
-			scheduledDateEndMinute,
-			deadlineDateYear,
-			deadlineDateMonth,
-			deadlineDateDay,
-			deadlineDateHour,
-			deadlineDateMinute,
-			belongings,
-			target,
-			other,
-			createdTime,
-			updatedTime,
-		)
+		create(item)
 		c.Redirect(302, "/ughfkhszdlvjkdjsbfkjsdabfl/sadmin")
 	})
 
+	// *************************************************
+	// url: "/ughfkhszdlvjkdjsbfkjsdabfl/sadmin/edit"
 	// sadmin Editページ
+	// *************************************************
 	r.GET("/ughfkhszdlvjkdjsbfkjsdabfl/sadmin/edit", func(c *gin.Context) {
 		r.LoadHTMLGlob("templates/sadmin/*")
 		db, err := gorm.Open("sqlite3", "test.sqlite3")
@@ -471,84 +376,63 @@ func main() {
 		}
 	})
 
+	// *************************************************
+	// url: "/ughfkhszdlvjkdjsbfkjsdabfl/sadmin/update"
 	// sadmin update
+	// *************************************************
 	r.POST("/ughfkhszdlvjkdjsbfkjsdabfl/sadmin/update", func(c *gin.Context) {
 		r.LoadHTMLGlob("templates/sadmin/*")
 
-		id, _ := strconv.Atoi(c.Query("id"))
-		title := c.PostForm("title")
-		description := c.PostForm("description")
-		price, _ := strconv.Atoi(c.PostForm("price"))
-		maxParticipants, _ := strconv.Atoi(c.PostForm("maxParticipants"))
-		numParticipants := 0
+		item := Item{}
+
+		id, _ := strconv.ParseUint(c.Query("id"), 10, 64)
+		item.ID = uint(id)
+		item.Title = c.PostForm("title")
+		item.Description = c.PostForm("description")
+		item.Price, _ = strconv.Atoi(c.PostForm("price"))
+		item.MaxParticipants, _ = strconv.Atoi(c.PostForm("maxParticipants"))
+		item.NumParticipants = 0
 
 		scheduledDate := c.PostForm("scheduledDate")
 		scheduledDateSlice := strings.Split(scheduledDate, "-")
-		scheduledDateYear, _ := strconv.Atoi(scheduledDateSlice[0])
-		scheduledDateMonth, _ := strconv.Atoi(scheduledDateSlice[1])
-		scheduledDateDay, _ := strconv.Atoi(scheduledDateSlice[2])
+		item.ScheduledDateYear, _ = strconv.Atoi(scheduledDateSlice[0])
+		item.ScheduledDateMonth, _ = strconv.Atoi(scheduledDateSlice[1])
+		item.ScheduledDateDay, _ = strconv.Atoi(scheduledDateSlice[2])
 
 		scheduledTime := c.PostForm("scheduledTime")
 		scheduledTimeSlice := strings.Split(scheduledTime, ":")
-		scheduledDateHour, _ := strconv.Atoi(scheduledTimeSlice[0])
-		scheduledDateMinute, _ := strconv.Atoi(scheduledTimeSlice[1])
+		item.ScheduledDateHour, _ = strconv.Atoi(scheduledTimeSlice[0])
+		item.ScheduledDateMinute, _ = strconv.Atoi(scheduledTimeSlice[1])
 
 		scheduledDateEnd := c.PostForm("scheduledDateEnd")
 		scheduledDateEndSlice := strings.Split(scheduledDateEnd, "-")
-		scheduledDateEndYear, _ := strconv.Atoi(scheduledDateEndSlice[0])
-		scheduledDateEndMonth, _ := strconv.Atoi(scheduledDateEndSlice[1])
-		scheduledDateEndDay, _ := strconv.Atoi(scheduledDateEndSlice[2])
+		item.ScheduledDateEndYear, _ = strconv.Atoi(scheduledDateEndSlice[0])
+		item.ScheduledDateEndMonth, _ = strconv.Atoi(scheduledDateEndSlice[1])
+		item.ScheduledDateEndDay, _ = strconv.Atoi(scheduledDateEndSlice[2])
 
 		scheduledEndTime := c.PostForm("scheduledEndTime")
 		scheduledEndTimeSlice := strings.Split(scheduledEndTime, ":")
-		scheduledDateEndHour, _ := strconv.Atoi(scheduledEndTimeSlice[0])
-		scheduledDateEndMinute, _ := strconv.Atoi(scheduledEndTimeSlice[1])
+		item.ScheduledDateEndHour, _ = strconv.Atoi(scheduledEndTimeSlice[0])
+		item.ScheduledDateEndMinute, _ = strconv.Atoi(scheduledEndTimeSlice[1])
 
 		deadlineDate := c.PostForm("deadlineDate")
 		deadlineDateSlice := strings.Split(deadlineDate, "-")
-		deadlineDateYear, _ := strconv.Atoi(deadlineDateSlice[0])
-		deadlineDateMonth, _ := strconv.Atoi(deadlineDateSlice[1])
-		deadlineDateDay, _ := strconv.Atoi(deadlineDateSlice[2])
+		item.DeadlineDateYear, _ = strconv.Atoi(deadlineDateSlice[0])
+		item.DeadlineDateMonth, _ = strconv.Atoi(deadlineDateSlice[1])
+		item.DeadlineDateDay, _ = strconv.Atoi(deadlineDateSlice[2])
 
 		deadlineTime := c.PostForm("deadlineTime")
 		deadlineTimeSlice := strings.Split(deadlineTime, ":")
-		deadlineDateHour, _ := strconv.Atoi(deadlineTimeSlice[0])
-		deadlineDateMinute, _ := strconv.Atoi(deadlineTimeSlice[1])
+		item.DeadlineDateHour, _ = strconv.Atoi(deadlineTimeSlice[0])
+		item.DeadlineDateMinute, _ = strconv.Atoi(deadlineTimeSlice[1])
 
-		belongings := c.PostForm("belongings")
-		target := c.PostForm("target")
-		other := c.PostForm("other")
-		createdTime := time.Now().Format("2006/1/2 15:04:05")
-		updatedTime := time.Now().Format("2006/1/2 15:04:05")
+		item.Belongings = c.PostForm("belongings")
+		item.Target = c.PostForm("target")
+		item.Other = c.PostForm("other")
+		item.CreatedTime = time.Now().Format("2006/1/2 15:04:05")
+		item.UpdatedTime = time.Now().Format("2006/1/2 15:04:05")
 
-		update(
-			id,
-			title,
-			description,
-			price,
-			maxParticipants,
-			numParticipants,
-			scheduledDateYear,
-			scheduledDateMonth,
-			scheduledDateDay,
-			scheduledDateHour,
-			scheduledDateMinute,
-			scheduledDateEndYear,
-			scheduledDateEndMonth,
-			scheduledDateEndDay,
-			scheduledDateEndHour,
-			scheduledDateEndMinute,
-			deadlineDateYear,
-			deadlineDateMonth,
-			deadlineDateDay,
-			deadlineDateHour,
-			deadlineDateMinute,
-			belongings,
-			target,
-			other,
-			createdTime,
-			updatedTime,
-		)
+		update(item)
 		c.Redirect(302, "/ughfkhszdlvjkdjsbfkjsdabfl/sadmin")
 	})
 
